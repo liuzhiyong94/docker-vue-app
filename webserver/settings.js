ec6ac1fcda8cade54f087397ae152ea816164aba
刘志勇 <liuzhiyong@whhcxw.cn>
2020-09-25 11:08:48 +0800

    settings

---
+++ webserver/settings.js
@@ -0,0 +1,20 @@
+﻿(function () {
+    module.exports = {
+        cookie_secret: 'secret_meteoric',
+        serverPort: 3010,
+        privateKey: 'hcxwwhrb2019',
+        mysql: {
+            database: 'ocrnationalserviceplatform',
+            host: '192.168.1.19',
+            user: 'assist',
+            password: 'ipcamera',
+            port: 3306,
+            connectionLimit: 120,
+            multipleStatements: true
+        },
+        FileUrl: "http://localhost:3000/",
+        FrontService: "http://localhost:3000",
+        tokenObj: {}
+    };
+}).call(this);
+
